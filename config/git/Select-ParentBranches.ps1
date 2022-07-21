927fbe009b27c3bfb905c5dd0cb8e430df59998d
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-07-21 14:14:17 -0500

    Refactor select parent-branches

--- config/git/Select-ParentBranches.ps1
+++ config/git/Select-ParentBranches.ps1
@@ -3,13 +3,16 @@
 . $PSScriptRoot/../branch-utils/Get-Tickets.ps1
 . $PSScriptRoot/Select-Branches.ps1
 
-function Select-ParentBranches([String]$branchName, [switch] $includeRemote) {
-    function ConvertTo-BranchName($branchInfo) {
-        return $includeRemote ? "$($branchInfo.remote)/$($branchInfo.branch)" : $branchInfo.branch
+function ConvertTo-BranchName($branchInfo, [switch] $includeRemote) {
+    return $includeRemote ? "$($branchInfo.remote)/$($branchInfo.branch)" : $branchInfo.branch
+}
+
+function Select-ParentBranches([String]$branchName, [PSObject[]] $allBranchInfo, [switch] $includeRemote) {
+    if ($allBranchInfo -eq $nil) {
+        $allBranchInfo = Select-Branches
     }
 
     # TODO: check "config branch" for things like RC branches
-    $branches = Select-Branches
 
     $info = ConvertTo-BranchInfo $branchName
 
@@ -22,26 +25,37 @@ function Select-ParentBranches([String]$branchName, [switch] $includeRemote) {
     }
     $parentTickets = $parentTickets | ForEach-Object {$_} # flatten the array
     if ($parentTickets.Length -eq 0) {
-        $serviceLines = @($branches | Where-Object { $_.type -eq 'service-line' } | ForEach-Object { ConvertTo-BranchName $_ })
+        $serviceLines = @($allBranchInfo | Where-Object { $_.type -eq 'service-line' } | ForEach-Object { ConvertTo-BranchName $_ -includeRemote:$includeRemote })
         if ($serviceLines.Length -gt 1) {
             $allLines = $serviceLines -join ' '
             throw "Found more than one service line ($serviceLines) - please specify the base."
         }
         return $serviceLines
     } else {
-        $possibleResult = $branches | Where-Object {
-            $tickets = (Get-Tickets $_)
-            if ($tickets.Length -eq 0) { return $false }
-            $intersection = $tickets | Where-Object { $parentTickets -contains $_ }
-            return $intersection.Length -eq $tickets.Length -AND $_.branch -ne $branchName
-        }
-
-        do {
-            $len = $possibleResult.Length
-            $parents = $possibleResult | ForEach-Object { return Select-ParentBranches $_.branch -includeRemote:$includeRemote } | ForEach-Object {$_} | select -uniq
-            $possibleResult = $possibleResult | Where-Object { $parents -notcontains (ConvertTo-BranchName $_) }
-        } until ($len -eq $possibleResult.Length)
-
-        return $possibleResult | ForEach-Object { ConvertTo-BranchName $_ }
+        return Invoke-SimplifyParentBranches (Invoke-ParentTicketsToBranches $parentTickets $allBranchInfo -includeRemote:$includeRemote) $allBranchInfo -includeRemote:$includeRemote
     }
+}
+
+function Invoke-ParentTicketsToBranches([string[]]$parentTickets, [PSObject[]] $allBranchInfo, [switch] $includeRemote) {
+    return $allBranchInfo | Where-Object {
+        $tickets = (Get-Tickets $_)
+        if ($tickets.Length -eq 0) { return $false }
+        $intersection = $tickets | Where-Object { $parentTickets -contains $_ }
+        return $intersection.Length -eq $tickets.Length -AND $_.branch -ne $branchName
+    } | ForEach-Object { ConvertTo-BranchName $_ -includeRemote:$includeRemote }
+}
+
+function Invoke-SimplifyParentBranches([string[]] $originalParents, [PSObject[]] $allBranchInfo, [switch] $includeRemote) {
+    if ($allBranchInfo -eq $nil) {
+        $allBranchInfo = Select-Branches
+    }
+
+    $possibleResult = $allBranchInfo | Where { $originalParents -contains (ConvertTo-BranchName $_ -includeRemote:$includeRemote) }
+    do {
+        $len = $possibleResult.Length
+        $parents = $possibleResult | ForEach-Object { return Select-ParentBranches $_.branch -includeRemote:$includeRemote } | ForEach-Object {$_} | select -uniq
+        $possibleResult = $possibleResult | Where-Object { $parents -notcontains (ConvertTo-BranchName $_ -includeRemote:$includeRemote) }
+    } until ($len -eq $possibleResult.Length)
+
+    return $possibleResult | ForEach-Object { ConvertTo-BranchName $_ -includeRemote:$includeRemote }
 }
