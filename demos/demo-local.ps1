6644b0c4ecd9c042c0067a55cd0d8f302cbf194c
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-08-12 16:04:47 -0500

    Add checks to local demo

--- demos/demo-local.ps1
+++ demos/demo-local.ps1
@@ -13,7 +13,22 @@ cd origin
 git new PS-1 -from main
 ThrowOnNativeFalure
 
+if ((git rev-parse main) -ne (git rev-parse HEAD)) {
+    throw 'HEAD does not point to the same commit as main';
+}
+
+if ((git branch --show-current) -ne 'feature/PS-1') {
+    throw 'Branch name did not match expected';
+}
+
 git rc -label 'test' -branches feature/add-item-1,feature/add-item-2
+ThrowOnNativeFalure
+
+if ((git branch --show-current) -ne 'feature/PS-1') {
+    throw 'Branch name should not have changed';
+}
+
+git verify-rc rc/test
 ThrowOnNativeFalure
 
 git branch > ../report.txt
