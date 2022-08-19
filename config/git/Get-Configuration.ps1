f7dac5baa05eb4dfe2160b28f40fb35b95e0a1bc
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-08-19 12:45:41 -0500

    Improve git-tool-config

--- config/git/Get-Configuration.ps1
+++ config/git/Get-Configuration.ps1
@@ -1,19 +1,30 @@
 . $PSScriptRoot/../core/coalesce.ps1
 
 function Get-Configuration() {
-    $remote = git config scaled-git.remote
-    if ($remote -eq $nil) {
-        $remote = git remote | Select-Object -First 1
-    }
+    $remote = Get-ConfiguredRemote
     return @{
         remote = $remote
-        upstreamBranch = (Coalesce (git config scaled-git.upstreamBranch) "_upstream")
-        defaultServiceLine = Get-DefaultServiceLine -remote $remote
+        upstreamBranch = Get-ConfiguredUpstreamBranch
+        defaultServiceLine = Get-ConfiguredDefaultServiceLine -remote $remote
     }
 }
 
-function Get-DefaultServiceLine([string]$remote) {
-    $result = (git config scaled-git.defaultServiceLine)
+function Get-ConfiguredRemote() {
+    $result = git config scaled-git.remote
+    if ($result -ne $nil) { return $result }
+    return git remote | Select-Object -First 1
+}
+
+function Get-ConfiguredUpstreamBranch() {
+    $result = git config scaled-git.upstreamBranch
+    if ($result -ne $nil) {
+        return $result;
+    }
+    return '_upstream'
+}
+
+function Get-ConfiguredDefaultServiceLine([string]$remote) {
+    $result = git config scaled-git.defaultServiceLine
     if ($result -ne $nil) { return $result }
 
     $commitish = git rev-parse --verify ($remote -eq $nil -OR $remote -eq '' ? 'main' : "$($remote)/main") -q 2> $nil
--- git-tool-config.ps1
+++ git-tool-config.ps1
@@ -1,12 +1,42 @@
 #!/usr/bin/env pwsh
 
 Param(
-    [Parameter()][String] $remote
+    [Parameter()][String] $remote,
+    [Parameter()][String] $upstreamBranch,
+    [Parameter()][String] $defaultServiceLine
 )
 
-$remotes = git remote
-if ($remote -ne '' -AND $remotes -notcontains $remote) {
-    throw "$remote not a valid remote for the repo."
-} elseif ($remote -ne '') {
-    git config scaled-git.remote $remote
+. $PSScriptRoot/config/git/Get-Configuration.ps1
+
+$oldConfig = Get-Configuration
+
+if ($remote -ne '') {
+    if ((git remote) -notcontains $remote) {
+        throw "$remote not a valid remote for the repo."
+    } else {
+        git config scaled-git.remote $remote
+    }
+    Write-Host "Set remote: $remote"
+} else {
+    $remote = $oldConfig.remote
+    Write-Host "Using previous remote: $remote"
+}
+
+if ($upstreamBranch -ne '') {
+    git config scaled-git.upstreamBranch $upstreamBranch
+    Write-Host "Set upstream: $upstreamBranch"
+} else {
+    Write-Host "Using previous upstream: $($oldConfig.upstreamBranch)"
+}
+
+if ($defaultServiceLine -ne '') {
+    $expected = $remote -eq $nil ? "$remote/$defaultServiceLine" : $defaultServiceLine
+    $branches = $remote -eq $nil ? (git branch --format '%(refname:short)') : (git branch -r --format '%(refname:short)')
+    if ($branches -notcontains $expected) {
+        throw "$expected is not found"
+    }
+    git config scaled-git.defaultServiceLine $defaultServiceLine
+    Write-Host "Set default service line: $defaultServiceLine"
+} else {
+    Write-Host "Using previous default service line: $($oldConfig.defaultServiceLine)"
 }
