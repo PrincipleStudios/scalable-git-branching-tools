4532f910bf22881fb6984540176966792beae4dc
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-10-04 15:57:18 -0500

    Add another release test

--- git-release.tests.ps1
+++ git-release.tests.ps1
@@ -22,7 +22,7 @@ BeforeAll {
 
     . $PSScriptRoot/config/git/Set-GitFiles.ps1
     Mock -CommandName Set-GitFiles {
-        throw "Unexpected parameters for Set-GitFiles: $(@{ files = $files; commitMessage = $commitMessage; branchName = $branchName; remote = $remote } | ConvertTo-Json)"
+        throw "Unexpected parameters for Set-GitFiles: $(@{ files = $files; commitMessage = $commitMessage; branchName = $branchName; remote = $remote; dryRun = $dryRun } | ConvertTo-Json)"
     }
 
     $noRemoteBranches = @(
@@ -226,6 +226,35 @@ Describe 'git-release' {
         Mock git -ParameterFilter $pushParameterFilter {} -Verifiable
 
         & $PSScriptRoot/git-release.ps1 rc/2022-07-14 main -dryrun
+    }
+    
+    It 'handles integration branches recursively' {
+        Mock-RemoteUpstream
+        Mock-UpdateGit
+
+        Mock git -ParameterFilter {($args -join ' ') -eq 'cat-file -p origin/_upstream:rc/2022-07-14'} {
+            "feature/FOO-123"
+            "integrate/FOO-125_XYZ-1"
+        }
+        
+        Mock -CommandName Set-GitFiles -ParameterFilter { 
+            $commitMessage -eq 'Release rc/2022-07-14 to main' -AND $branchName -eq '_upstream' -AND $remote -eq 'origin' -AND $dryRun `
+                -AND $files['feature/FOO-123'] -eq $nil `
+                -AND $files['integrate/FOO-125_XYZ-1'] -eq $nil `
+                -AND $files['rc/2022-07-14'] -eq $nil `
+                -AND $files['feature/XYZ-1-services'] -eq $nil `
+                -AND $files['feature/FOO-124_FOO-125'] -eq $nil `
+                -AND $files['feature/FOO-124-comment'] -eq $nil
+        } { 
+            'new-commit'
+        }
+
+        $pushParameterFilter = {($args -join ' ') -eq 'push --atomic origin origin/rc/2022-07-14:main :feature/FOO-123 :integrate/FOO-125_XYZ-1 :feature/FOO-124_FOO-125 :feature/XYZ-1-services :feature/FOO-124-comment :rc/2022-07-14 new-commit:_upstream'}
+        Mock git -ParameterFilter $pushParameterFilter {} -Verifiable
+
+        & $PSScriptRoot/git-release.ps1 rc/2022-07-14 main
+
+        Should -Invoke -CommandName git -Times 1 -ParameterFilter $pushParameterFilter
     }
     
 }
