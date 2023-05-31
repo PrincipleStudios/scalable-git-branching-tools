1fd45e9a6c9767ad7b8f2e3ad26f71d42ed490b3
Matt DeKrey <mattdekrey@gmail.com>
2023-05-31 10:17:19 -0500

    Add function to simplify upstream branches

---
+++ config/git/Compress-UpstreamBranches.psm1
@@ -0,0 +1,20 @@
+. $PSScriptRoot/../core/ArrayToHash.ps1
+Import-Module -Scope Local "$PSScriptRoot/Select-UpstreamBranches.psm1"
+
+function Compress-UpstreamBranches([Parameter(Mandatory)][AllowEmptyCollection()][string[]] $originalUpstream) {
+    $allUpstream = $originalUpstream | ArrayToHash -getValue {
+        return ([string[]](Select-UpstreamBranches $_ -recurse))
+    }
+    $resultUpstream = [System.Collections.ArrayList]$originalUpstream
+    for ($i = 0; $i -lt $resultUpstream.Count; $i++) {
+        $branch = $resultUpstream[$i]
+        if (($resultUpstream | Where-Object { $_ -ne $branch -AND $allUpstream[$_] -contains $branch }) -ne $nil) {
+            # $branch is in the recursive upstream of at least one other branch
+            $resultUpstream.Remove($branch)
+            $i--
+        }
+    }
+    return [string[]]$resultUpstream
+}
+
+Export-ModuleMember -Function Compress-UpstreamBranches
