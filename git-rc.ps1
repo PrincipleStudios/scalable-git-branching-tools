7739340ab341294c314f7a6b10161bbe9c92d5c0
Matt DeKrey <mattdekrey@gmail.com>
2023-06-01 10:10:40 -0500

    Correct erroneous import

--- git-rc.ps1
+++ git-rc.ps1
@@ -21,7 +21,7 @@ Import-Module -Scope Local "$PSScriptRoot/config/git/Invoke-PreserveBranch.psm1"
 Import-Module -Scope Local "$PSScriptRoot/config/git/Invoke-CreateBranch.psm1"
 Import-Module -Scope Local "$PSScriptRoot/config/git/Invoke-CheckoutBranch.psm1";
 Import-Module -Scope Local "$PSScriptRoot/config/git/Invoke-MergeBranches.psm1";
-Import-Module -Scope Local "$PSScriptRoot/config/git/Set-MultipleUpstreamBranches.mocks.psm1"
+Import-Module -Scope Local "$PSScriptRoot/config/git/Set-MultipleUpstreamBranches.psm1"
 
 $config = Get-Configuration
 Update-Git
