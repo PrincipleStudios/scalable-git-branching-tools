3bcbbfaf9734f386096cab6f6d6d8d60a9b4dbfa
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-09-01 10:57:12 -0500

    Add more tests

--- utils/diagnostics/diagnostic-framework.tests.ps1
+++ utils/diagnostics/diagnostic-framework.tests.ps1
@@ -17,6 +17,35 @@ Describe 'diagnostic-framework' {
             $output | Should -Contain 'WARN: Warning 2'
             $output | Should -Contain 'ERR:  Error 1'
         }
+
+        It 'does not throw if there are no errors' {
+            $diag = New-Diagnostics
+            Add-WarningDiagnostic $diag 'Warning 1'
+            Add-WarningDiagnostic $diag 'Warning 2'
+            $output = Register-Diagnostics -throwInsteadOfExit
+            { Assert-Diagnostics $diag } | Should -Not -Throw
+
+            $output | Should -Contain 'WARN: Warning 1'
+            $output | Should -Contain 'WARN: Warning 2'
+        }
+
+        It 'can be empty' {
+            $diag = New-Diagnostics
+            $output = Register-Diagnostics -throwInsteadOfExit
+            { Assert-Diagnostics $diag } | Should -Not -Throw
+
+            $output | Should -BeNullOrEmpty
+        }
+    }
+
+    Context 'when diagnostics are not passed' {
+        It 'throws on warnings' {
+            { Add-WarningDiagnostic $nil 'Warning 1' } | Should -Throw
+        }
+
+        It 'throws on errors' {
+            { Add-ErrorDiagnostic $nil 'Error 1' } | Should -Throw
+        }
     }
 
 }
