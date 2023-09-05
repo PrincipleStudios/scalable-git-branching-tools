0af696cfa5f929e33852c875ba2aa60e0d02bc4d
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-09-05 08:28:09 -0500

    Add polyfill files to prevent modifying many

---
+++ config/git/Get-Configuration.mocks.psm1
@@ -0,0 +1,2 @@
+Import-Module -Scope Local "$PSScriptRoot/../../utils/query-state/Configuration.mocks.psm1"
+Export-ModuleMember -Function Initialize-ToolConfiguration
---
+++ config/git/Get-Configuration.psm1
@@ -0,0 +1,2 @@
+Import-Module -Scope Local "$PSScriptRoot/../../utils/query-state/Configuration.psm1"
+Export-ModuleMember -Function Get-Configuration
