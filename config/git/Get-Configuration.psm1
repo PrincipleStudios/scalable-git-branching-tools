9939e2cb7eff18e709c098ee4293c669f8605893
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-09-05 08:14:30 -0500

    Correct warning

--- config/git/Get-Configuration.psm1
+++ config/git/Get-Configuration.psm1
@@ -28,7 +28,7 @@ function Get-ConfiguredDefaultServiceLine([string]$remote) {
     $result = git config scaled-git.defaultServiceLine
     if ($result -ne $nil) { return $result }
 
-    $commitish = git rev-parse --verify ($remote -eq $nil -OR $remote -eq '' ? 'main' : "$($remote)/main") -q 2> $nil
+    git rev-parse --verify ($remote -eq $nil -OR $remote -eq '' ? 'main' : "$($remote)/main") -q > $nil 2> $nil
     if ($LASTEXITCODE -eq 0) {
         return "main"
     }
