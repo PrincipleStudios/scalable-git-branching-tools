117050618958897180f98dc24a639a3630af1ea7
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-09-25 15:44:05 -0500

    Add `Show-ProcessLogs`

--- utils/framework.psm1
+++ utils/framework.psm1
@@ -2,4 +2,4 @@ Import-Module -Scope Local "$PSScriptRoot/framework/diagnostic-framework.psm1"
 Import-Module -Scope Local "$PSScriptRoot/framework/processlog-framework.psm1"
 
 Export-ModuleMember -Function New-Diagnostics, Add-ErrorDiagnostic, Add-WarningDiagnostic, Assert-Diagnostics `
-    , Invoke-ProcessLogs
+    , Invoke-ProcessLogs, Show-ProcessLogs
