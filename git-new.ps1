5690e16d901a234d04f568163942c5e7ee9ef450
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-10-11 14:18:59 -0500

    Add inline documentation

--- git-new.ps1
+++ git-new.ps1
@@ -1,5 +1,21 @@
 #!/usr/bin/env pwsh
 
+<#
+.SYNOPSIS
+    Creates a new branch and checks it out from the specified branches
+
+.PARAMETER branchName
+    Specifies the name of the branch.
+
+.PARAMETER comment
+    Specifies a comment as part of the commit message for the upstream branch.
+
+.PARAMETER upstreamBranches
+    A comma-delimited list of branches (without the remote, if applicable). If not specified, assumes the default service line (see [tool-config](./tool-config.md).)
+
+.PARAMETER dryRun
+    If specified, only test merging, do not push the updates.
+#>
 Param(
     [Parameter(Mandatory)][String] $branchName,
     [Parameter()][Alias('m')][Alias('message')][ValidateLength(1,25)][String] $comment,
