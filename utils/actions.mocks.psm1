cc9ac57be71c391f2a78e154a3cc95ecd8449574
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-10-16 12:14:47 -0500

    Add missing local action mocks

--- utils/actions.mocks.psm1
+++ utils/actions.mocks.psm1
@@ -1,3 +1,9 @@
+Import-Module -Scope Local "$PSScriptRoot/actions/local/Register-LocalActionAssertPushed.mocks.psm1"
+Export-ModuleMember -Function Initialize-LocalActionAssertPushedNotTracked, Initialize-LocalActionAssertPushedSuccess, Initialize-LocalActionAssertPushedAhead
+
+Import-Module -Scope Local "$PSScriptRoot/actions/local/Register-LocalActionGetUpstream.mocks.psm1"
+Export-ModuleMember -Function Initialize-AnyUpstreamBranches,Initialize-UpstreamBranches
+
 Import-Module -Scope Local "$PSScriptRoot/actions/local/Register-LocalActionSetUpstream.mocks.psm1"
 Export-ModuleMember -Function Lock-LocalActionSetUpstream, Initialize-LocalActionSetUpstream
 
@@ -6,6 +12,9 @@ Export-ModuleMember -Function Initialize-LocalActionCreateBranchSuccess
 
 Import-Module -Scope Local "$PSScriptRoot/actions/local/Register-LocalActionSimplifyUpstreamBranches.mocks.psm1"
 Export-ModuleMember -Function Initialize-LocalActionSimplifyUpstreamBranchesSuccess
+
+Import-Module -Scope Local "$PSScriptRoot/actions/local/Register-LocalActionValidateBranchNames.mocks.psm1"
+Export-ModuleMember -Function Initialize-LocalActionValidateBranchNamesSuccess
 
 Import-Module -Scope Local "$PSScriptRoot/actions/finalize/Register-FinalizeActionCheckout.mocks.psm1"
 Export-ModuleMember -Function Initialize-FinalizeActionCheckout
