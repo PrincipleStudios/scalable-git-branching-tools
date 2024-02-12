107a3a10ec40ec019daf1074630c54b6ebaaaa54
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-02-12 14:12:40 -0600

    Improve show-upstream tests

--- git-show-upstream.tests.ps1
+++ git-show-upstream.tests.ps1
@@ -7,37 +7,131 @@ BeforeAll {
 }
 
 Describe 'git-show-upstream' {
-    It 'shows the results of an upstream branch' {
-        Initialize-ToolConfiguration
-        Initialize-UpstreamBranches @{ 'feature/FOO-123' = @("main", "infra/add-services") }
-
-        $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -target 'feature/FOO-123'
-        $result | Should -Be @('origin/main', 'origin/infra/add-services')
-    }
-
-    It 'shows the results of the current branch if none is specified' {
-        Initialize-ToolConfiguration
-        Initialize-CurrentBranch 'feature/FOO-123'
-
-        Initialize-UpstreamBranches @{ 'feature/FOO-123' = @("main", "infra/add-services") }
-
-        $result = & ./git-show-upstream.ps1 -noFetch -includeRemote
-        $result | Should -Be @('origin/main', 'origin/infra/add-services')
-    }
-
-    It 'shows recursive the results of the current branch if none is specified' {
-        Initialize-ToolConfiguration
-        Initialize-CurrentBranch 'feature/FOO-123'
-        Initialize-UpstreamBranches @{
-            'feature/FOO-123' = $("main", "infra/add-services")
-            'main' = $()
-            'infra/add-services' = $('infra/build-infrastructure')
-            'infra/build-infrastructure' = $()
+    Describe 'with a remote' {
+        # Scenario:
+        # - remote configured
+        # - upstreams include:
+        #     feature/FOO-123 = main & infra/add-services
+        #     main = none
+        #     infra/add-services = infra/build-infrastructure
+        #     infra/build-infrastructure = none
+        # - no current branch
+        BeforeEach {
+            Initialize-ToolConfiguration
+            Initialize-UpstreamBranches @{
+                'feature/FOO-123' = $("main", "infra/add-services")
+                'main' = $()
+                'infra/add-services' = $('infra/build-infrastructure')
+                'infra/build-infrastructure' = $()
+            }
         }
 
-        $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -recurse
-        $result | Should -Be @('origin/main', 'origin/infra/add-services', 'origin/infra/build-infrastructure')
-    }
+        It 'shows the results of an upstream branch' {
+            $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -target 'feature/FOO-123'
 
-    # TODO - improve tests, but shows that the old behavior can still be invoked
+            $result | Should -Be @('origin/main', 'origin/infra/add-services')
+        }
+
+        Describe 'when using the current branch' {
+            # Scenario, as above, with:
+            # - current branch is feature/FOO-123
+            BeforeEach {
+                Initialize-CurrentBranch 'feature/FOO-123'
+            }
+
+            It 'shows the results of the current branch if none is specified and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote
+                $result | Should -Be @('origin/main', 'origin/infra/add-services')
+            }
+
+            It 'allows specifying the branch with arguments and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -target infra/add-services
+                $result | Should -Be @('origin/infra/build-infrastructure')
+            }
+
+            It 'shows recursive the results of the current branch if none is specified and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -recurse
+                $result | Should -Be @('origin/main', 'origin/infra/add-services', 'origin/infra/build-infrastructure')
+            }
+            
+            It 'shows the results of the current branch if none is specified' {
+                $result = & ./git-show-upstream.ps1 -noFetch
+                $result | Should -Be @('main', 'infra/add-services')
+            }
+
+            It 'allows specifying the branch with arguments' {
+                $result = & ./git-show-upstream.ps1 -noFetch -target infra/add-services
+                $result | Should -Be @('infra/build-infrastructure')
+            }
+
+            It 'shows recursive the results of the current branch if none is specified' {
+                $result = & ./git-show-upstream.ps1 -noFetch -recurse
+                $result | Should -Be @('main', 'infra/add-services', 'infra/build-infrastructure')
+            }
+        }
+    }
+    
+    Describe 'without a remote' {
+        # Scenario:
+        # - remote configured
+        # - upstreams include:
+        #     feature/FOO-123 = main & infra/add-services
+        #     main = none
+        #     infra/add-services = infra/build-infrastructure
+        #     infra/build-infrastructure = none
+        # - no current branch
+        BeforeEach {
+            Initialize-ToolConfiguration -noRemote
+            Initialize-UpstreamBranches @{
+                'feature/FOO-123' = $("main", "infra/add-services")
+                'main' = $()
+                'infra/add-services' = $('infra/build-infrastructure')
+                'infra/build-infrastructure' = $()
+            }
+        }
+
+        It 'shows the results of an upstream branch' {
+            $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -target 'feature/FOO-123'
+
+            $result | Should -Be @('main', 'infra/add-services')
+        }
+
+        Describe 'when using the current branch' {
+            # Scenario, as above, with:
+            # - current branch is feature/FOO-123
+            BeforeEach {
+                Initialize-CurrentBranch 'feature/FOO-123'
+            }
+
+            It 'shows the results of the current branch if none is specified and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote
+                $result | Should -Be @('main', 'infra/add-services')
+            }
+
+            It 'allows specifying the branch with arguments and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -target infra/add-services
+                $result | Should -Be @('infra/build-infrastructure')
+            }
+
+            It 'shows recursive the results of the current branch if none is specified and including remote in the response' {
+                $result = & ./git-show-upstream.ps1 -noFetch -includeRemote -recurse
+                $result | Should -Be @('main', 'infra/add-services', 'infra/build-infrastructure')
+            }
+            
+            It 'shows the results of the current branch if none is specified' {
+                $result = & ./git-show-upstream.ps1 -noFetch
+                $result | Should -Be @('main', 'infra/add-services')
+            }
+
+            It 'allows specifying the branch with arguments' {
+                $result = & ./git-show-upstream.ps1 -noFetch -target infra/add-services
+                $result | Should -Be @('infra/build-infrastructure')
+            }
+
+            It 'shows recursive the results of the current branch if none is specified' {
+                $result = & ./git-show-upstream.ps1 -noFetch -recurse
+                $result | Should -Be @('main', 'infra/add-services', 'infra/build-infrastructure')
+            }
+        }
+    }
 }
