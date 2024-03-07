5e59666580334a64562c8032a5f32830527c7680
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-07 14:33:36 -0600

    Improve stubbed tests

--- git-refactor-upstream.tests.ps1
+++ git-refactor-upstream.tests.ps1
@@ -6,7 +6,7 @@ Describe 'git-refactor-upstream' {
         Import-Module -Scope Local "$PSScriptRoot/utils/query-state.mocks.psm1"
         Import-Module -Scope Local "$PSScriptRoot/utils/actions.mocks.psm1"
     }
-    
+
     BeforeEach {
         Initialize-ToolConfiguration
     }
@@ -19,6 +19,11 @@ Describe 'git-refactor-upstream' {
             'feature/XYZ-1-services' = @("main")
             'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
         }
+
+        & $PSScriptRoot/git-refactor-upstream.ps1 -source 'feature/FOO-123' -target 'main' -remove
+
+        $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
+        # Invoke-VerifyMock $mocks -Times 1
     } -Pending
 
     It 'can consolidate an integration branch (integrate/FOO-123_XYZ-1) into its remaining upstream' {
@@ -28,15 +33,25 @@ Describe 'git-refactor-upstream' {
             'feature/XYZ-1-services' = @("main")
             'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
         }
+
+        & $PSScriptRoot/git-refactor-upstream.ps1 -source 'integrate/FOO-123_XYZ-1' -target 'feature/XYZ-1-services' -remove
+        
+        $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
+        # Invoke-VerifyMock $mocks -Times 1
     } -Pending
 
     It 'can rename an incorrectly named branch' {
         Initialize-AllUpstreamBranches @{
             'integrate/FOO-100_XYZ-1' = @("feature/FOO-123", "feature/XYZ-1-services")
             'feature/FOO-124' = @("integrate/FOO-100_XYZ-1")
-            'feature/FOO-100' = @("main")
+            'feature/FOO-123' = @("main")
             'feature/XYZ-1-services' = @("main")
             'rc/1.1.0' = @("integrate/FOO-100_XYZ-1")
         }
+
+        & $PSScriptRoot/git-refactor-upstream.ps1 -source 'integrate/FOO-100_XYZ-1' -target 'integrate/FOO-123_XYZ-1' -rename
+        
+        $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
+        # Invoke-VerifyMock $mocks -Times 1
     } -Pending
 }
