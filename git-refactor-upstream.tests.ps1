0dfc7eb63039bb1f395c6a093946c942d1a400dc
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-08 09:05:48 -0600

    Add basic refactor-upstream implementation

--- git-refactor-upstream.tests.ps1
+++ git-refactor-upstream.tests.ps1
@@ -8,50 +8,89 @@ Describe 'git-refactor-upstream' {
     }
 
     BeforeEach {
+        [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUserDeclaredVarsMoreThanAssignments', '', Justification='This is put in scope and used in the tests below')]
+        $fw = Register-Framework
+
         Initialize-ToolConfiguration
+        Initialize-UpdateGitRemote
     }
 
     It 'can consolidate a released branch (feature/FOO-123) into main' {
-        Initialize-AllUpstreamBranches @{
-            'integrate/FOO-123_XYZ-1' = @("feature/FOO-123", "feature/XYZ-1-services")
-            'feature/FOO-124' = @("integrate/FOO-123_XYZ-1")
-            'feature/FOO-123' = @("main")
-            'feature/XYZ-1-services' = @("main")
-            'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
-        }
+        $mocks = @(
+            Initialize-AllUpstreamBranches @{
+                'integrate/FOO-123_XYZ-1' = @("feature/FOO-123", "feature/XYZ-1-services")
+                'feature/FOO-124' = @("integrate/FOO-123_XYZ-1")
+                'feature/FOO-123' = @("main")
+                'feature/XYZ-1-services' = @("main")
+                'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
+            }
+            Initialize-AssertValidBranchName 'feature/FOO-123'
+            Initialize-AssertValidBranchName 'main'
+            Initialize-LocalActionSimplifyUpstreamBranchesSuccess @('feature/XYZ-1-services', 'main') @('feature/XYZ-1-services')
+            Initialize-LocalActionSetUpstream @{
+                'integrate/FOO-123_XYZ-1' = @("feature/XYZ-1-services")
+            } -commitish 'new-commit'
+            Initialize-FinalizeActionSetBranches @{
+                _upstream = 'new-commit'
+            }
+        )
 
         & $PSScriptRoot/git-refactor-upstream.ps1 -source 'feature/FOO-123' -target 'main' -remove
 
         $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
-        # Invoke-VerifyMock $mocks -Times 1
-    } -Pending
+        Invoke-VerifyMock $mocks -Times 1
+    }
 
     It 'can consolidate an integration branch (integrate/FOO-123_XYZ-1) into its remaining upstream' {
-        Initialize-AllUpstreamBranches @{
-            'integrate/FOO-123_XYZ-1' = @("feature/XYZ-1-services")
-            'feature/FOO-124' = @("integrate/FOO-123_XYZ-1")
-            'feature/XYZ-1-services' = @("main")
-            'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
-        }
+        $mocks = @(
+            Initialize-AllUpstreamBranches @{
+                'integrate/FOO-123_XYZ-1' = @("feature/XYZ-1-services")
+                'feature/FOO-124' = @("integrate/FOO-123_XYZ-1")
+                'feature/XYZ-1-services' = @("main")
+                'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
+            }
+            Initialize-AssertValidBranchName 'integrate/FOO-123_XYZ-1'
+            Initialize-AssertValidBranchName 'feature/XYZ-1-services'
+            Initialize-LocalActionSimplifyUpstreamBranchesSuccess @('feature/XYZ-1-services') @('feature/XYZ-1-services')
+            Initialize-LocalActionSetUpstream @{
+                'feature/FOO-124' = @("feature/XYZ-1-services")
+                'rc/1.1.0' = @("feature/XYZ-1-services")
+            } -commitish 'new-commit'
+            Initialize-FinalizeActionSetBranches @{
+                _upstream = 'new-commit'
+            }
+        )
 
         & $PSScriptRoot/git-refactor-upstream.ps1 -source 'integrate/FOO-123_XYZ-1' -target 'feature/XYZ-1-services' -remove
         
         $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
-        # Invoke-VerifyMock $mocks -Times 1
-    } -Pending
+        Invoke-VerifyMock $mocks -Times 1
+    }
 
     It 'can rename an incorrectly named branch' {
-        Initialize-AllUpstreamBranches @{
-            'integrate/FOO-100_XYZ-1' = @("feature/FOO-123", "feature/XYZ-1-services")
-            'feature/FOO-124' = @("integrate/FOO-100_XYZ-1")
-            'feature/FOO-123' = @("main")
-            'feature/XYZ-1-services' = @("main")
-            'rc/1.1.0' = @("integrate/FOO-100_XYZ-1")
-        }
+        $mocks = @(
+            Initialize-AllUpstreamBranches @{
+                'integrate/FOO-100_XYZ-1' = @("feature/FOO-123", "feature/XYZ-1-services")
+                'feature/FOO-124' = @("integrate/FOO-100_XYZ-1")
+                'feature/FOO-123' = @("main")
+                'feature/XYZ-1-services' = @("main")
+                'rc/1.1.0' = @("integrate/FOO-100_XYZ-1")
+            }
+            Initialize-AssertValidBranchName 'integrate/FOO-100_XYZ-1'
+            Initialize-AssertValidBranchName 'integrate/FOO-123_XYZ-1'
+            Initialize-LocalActionSimplifyUpstreamBranchesSuccess @('integrate/FOO-123_XYZ-1') @('integrate/FOO-123_XYZ-1')
+            Initialize-LocalActionSetUpstream @{
+                'feature/FOO-124' = @("integrate/FOO-123_XYZ-1")
+                'rc/1.1.0' = @("integrate/FOO-123_XYZ-1")
+            } -commitish 'new-commit'
+            Initialize-FinalizeActionSetBranches @{
+                _upstream = 'new-commit'
+            }
+        )
 
         & $PSScriptRoot/git-refactor-upstream.ps1 -source 'integrate/FOO-100_XYZ-1' -target 'integrate/FOO-123_XYZ-1' -rename
         
         $fw.assertDiagnosticOutput | Should -BeNullOrEmpty
-        # Invoke-VerifyMock $mocks -Times 1
-    } -Pending
+        Invoke-VerifyMock $mocks -Times 1
+    }
 }
