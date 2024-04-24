50990cb3423a040e25fca688eea66ae33905a3e5
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-04-24 11:05:19 -0500

    Remove unused Invoke-CreateBranch

--- utils/git.psm1
+++ utils/git.psm1
@@ -3,7 +3,6 @@ Import-Module -Scope Local "$PSScriptRoot/git/Invoke-WriteTree.psm1"
 Import-Module -Scope Local "$PSScriptRoot/git/Set-GitFiles.psm1"
 Import-Module -Scope Local "$PSScriptRoot/git/Invoke-PreserveBranch.psm1"
 Import-Module -Scope Local "$PSScriptRoot/git/Invoke-CheckoutBranch.psm1"
-Import-Module -Scope Local "$PSScriptRoot/git/Invoke-CreateBranch.psm1"
 Import-Module -Scope Local "$PSScriptRoot/git/Set-RemoteTracking.psm1"
 
 Export-ModuleMember -Function Invoke-WriteBlob `
@@ -11,7 +10,6 @@ Export-ModuleMember -Function Invoke-WriteBlob `
     , Set-GitFiles `
     , Invoke-PreserveBranch, New-ResultAfterCleanup, Get-GitHead, Restore-GitHead `
     , Invoke-CheckoutBranch `
-    , Invoke-CreateBranch `
     , Set-RemoteTracking
 
 Import-Module -Scope Local "$PSScriptRoot/git/Invoke-MergeTogether.psm1"
