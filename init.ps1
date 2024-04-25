a0bacdd15ad2eb611869555e9fb0eedd7ac519b7
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-04-25 08:12:03 -0500

    Remove RCI command (unused by team)

--- init.ps1
+++ init.ps1
@@ -29,9 +29,6 @@ git config alias.add-upstream "!$dir/git-add-upstream.ps1"
 # Build a release candidate from other branches
 git config alias.rc "!$dir/git-rc.ps1"
 
-# Build a release candidate from other branches using interactive prompts
-git config alias.rci "!$dir/git-rci.ps1"
-
 # Rebuild a branch from its upstreams
 git config alias.rebuild-rc "!$dir/git-rebuild-rc.ps1"
 
